4f25801c1fcbb589f74165aec69ac881f27f779e
Kiss Anikó <126045521+KissAniko@users.noreply.github.com>
2023-03-13 14:14:21 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,67 @@ namespace sudokuGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int inputNumber;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(txtNumber.Text) < 9)
+            {
+                inputNumber = Convert.ToInt32(txtNumber.Text);
+                inputNumber += 1;
+                txtNumber.Text = Convert.ToString(inputNumber);
+            }
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            
+                if (Convert.ToInt32(txtNumber.Text) > 4)
+                {
+                    inputNumber = Convert.ToInt32(txtNumber.Text);
+                    inputNumber--;
+                    txtNumber.Text = Convert.ToString(inputNumber);
+                }
+            
+        }
+
+        private void txtNumber_ContextMenuClosing(object sender, ContextMenuEventArgs e)
+        {
+            if (txtNumber.Text.Length == 0 || Convert.ToByte(txtNumber.Text) > 9 || Convert.ToByte(txtNumber.Text) < 4)
+            {
+                txtNumber.Text = "4";
+            }
+        }
+
+        private void txtKezdoallapot_ContextMenuClosing(object sender, ContextMenuEventArgs e)
+        {
+           if (txtKezdoallapot.Text.Length == Math.Pow(Convert.ToDouble(txtNumber.Text), 2))
+            {
+                MessageBox.Show("A feladvány megfelelő hosszúságú!");
+            }
+            else if (txtKezdoallapot.Text.Length > Convert.ToInt32(txtNumber.Text))
+            {
+                MessageBox.Show($"A feladvány hosszú: törlendő {txtKezdoallapot.Text.Length - Math.Pow(Convert.ToDouble(txtNumber.Text), 2)} számjegy!");
+            }
+            else
+            {
+                MessageBox.Show($"A feladvány rövid: kell még {Math.Pow(Convert.ToDouble(txtNumber.Text), 2) - txtKezdoallapot.Text.Length} számjegy!");
+            }
+        }
+
+        private void lblHossz_ContextMenuClosing(object sender, ContextMenuEventArgs e)
+        {
+            lblHossz.Content = "Hossz: " + txtKezdoallapot.Text.Length;
+        }
+
+        private void btnEllenorzes_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
